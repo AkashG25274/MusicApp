7479dff5b7bb95e04fc14ae69116135b583d33e7
Akash Gaikwad <jithin.nair@mediamagictechnologies.com>
2020-07-16 19:20:53 +0530

    Placed album tab to its previous place as changed it for testing

--- MusicApp/View Controllers/MainTabBarController.m
+++ MusicApp/View Controllers/MainTabBarController.m
@@ -45,7 +45,7 @@
         playlistsTabNavigationController.tabBarItem.title = playlistTabBarTitle;
         [playlistsTabNavigationController.tabBarItem setTitleTextAttributes:@{NSFontAttributeName: [UIFont boldSystemFontOfSize:15.0], NSForegroundColorAttributeName: [UIColor blueColor]} forState:UIControlStateNormal];
         
-        self.viewControllers = @[albumsTabNavigationController, trackTabNavigationController, artistsTabNavigationController, playlistsTabNavigationController];
+        self.viewControllers = @[trackTabNavigationController, albumsTabNavigationController, artistsTabNavigationController, playlistsTabNavigationController];
     }
     
     return self;
